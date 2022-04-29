5d688886b391df7f03f7de329d6d0feacaefec03
sami92news <sami.akram@92newshd.tv>
2022-04-29 12:13:59 +0500

    no thumbnails

--- djcms/settings.py
+++ djcms/settings.py
@@ -195,8 +195,9 @@ DATABASES = {
 
 THUMBNAIL_PROCESSORS = (
     'easy_thumbnails.processors.colorspace',
-    'easy_thumbnails.processors.autocrop',
-    'filer.thumbnail_processors.scale_and_crop_with_subject_location',
+    # 'easy_thumbnails.processors.autocrop',
+    # 'filer.thumbnail_processors.scale_and_crop_with_subject_location',
+    'news.thumbnail_processors.scale_and_crop_with_subject_location',
     'easy_thumbnails.processors.filters'
 )
 
---
+++ news/thumbnail_processors.py
@@ -0,0 +1,6 @@
+def scale_and_crop_with_subject_location(im, size, subject_location=False,
+                                         zoom=None, crop=False, upscale=False,
+                                         **kwargs):
+    return im
+
+
